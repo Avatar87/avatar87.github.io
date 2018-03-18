df3d355db4675380672afce0849e807bb4e0612d
Avatar87 <36476272+Avatar87@users.noreply.github.com>
2018-03-18 22:56:51 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,50 +1,25 @@
 
 	$(document).ready(function(){
+    	v = $( ".slider" ).css('width');
+    	console.log(v);
+
+    $(window).on('resize', function () {
+    	v = $( ".slider" ).css('width');
+	});
+
 	$('.scrollright').click(function()
 	{
 
-    $('#scrollbar').animate( { scrollLeft: '+=1230' }, 1000);
+    $('#scrollbar').animate( { scrollLeft: '+=' + v }, 1000);
 	});
 
 	$('.scrollleft').click(function()
 	{
-    $('#scrollbar').animate( { scrollLeft: '-=1230' }, 1000);
-	});
-	
-	$('.scrollright').click(function()
-	{
 
-    $('#scrollbar2').animate( { scrollLeft: '+=460' }, 1000);
+    $('#scrollbar').animate( { scrollLeft: '-=' + v }, 1000);
 	});
 
-	$('.scrollleft').click(function()
-	{
-    $('#scrollbar2').animate( { scrollLeft: '-=460' }, 1000);
 	});
-	$('.scrollright2').click(function()
-	{
-
-    $('#scrollbar3').animate( { scrollLeft: '+=460' }, 1000);
-	});
-
-	$('.scrollleft2').click(function()
-	{
-    $('#scrollbar3').animate( { scrollLeft: '-=460' }, 1000);
-	});
-	
-	$('.scrollright2').click(function()
-	{
-
-    $('#scrollbar4').animate( { scrollLeft: '+=460' }, 1000);
-	});
-
-	$('.scrollleft2').click(function()
-	{
-    $('#scrollbar4').animate( { scrollLeft: '-=460' }, 1000);
-	});
-	});
-
-
 
 
 
